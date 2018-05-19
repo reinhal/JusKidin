db96c23f5543cd70750f41f84f29b30b1670dab2
Lisa Reinhardt <reinal@gmail.com>
2018-05-19 17:41:04 -0500

    added one test to verify the root url

--- test/test.js
+++ test/test.js
@@ -2,5 +2,18 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const faker = require('faker');
-const mongoose = require('mongoose');
+const app = require('../server.js');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('hello world!', function () {
+  it('should exist', function () {
+    return chai.request(app)
+      .get('/')
+      .then(function (res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
