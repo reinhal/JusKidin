e69ab416b03f9aaf3b34ebccf930a56f7f16073b
Lisa Reinhardt <reinhal@gmail.com>
2018-05-26 08:04:48 -0500

    updated server file to include runServer and closeServer functions

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../server.js');
+const {app, runServer, closeServer} = require('../server.js');
 
 const expect = chai.expect;
 
