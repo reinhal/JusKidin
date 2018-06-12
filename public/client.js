564eda11f0465ff7c2063213eaa5be86fad559ef
Lisa Reinhardt <reinal@gmail.com>
2018-06-12 11:57:31 -0500

    added client.js file to the public folder

---
+++ public/client.js
@@ -0,0 +1,73 @@
+
+///////////// Overlay Form Functions ///////////////////////
+
+
+function childOn() {
+    document.getElementById("childoverlay").style.display = "block";
+}
+
+function childOff() {
+    document.getElementById("childoverlay").style.display = "none";
+}
+
+function assetOn() {
+    document.getElementById("assetoverlay").style.display = "block";
+}
+
+function assetOff() {
+    document.getElementById("assetoverlay").style.display = "none";
+}
+
+function drawerOn() {
+    document.getElementById("draweroverlay").style.display = "block";
+}
+
+function drawerOff() {
+    document.getElementById("draweroverlay").style.display = "none";
+}
+
+///////////// JusKidin Functions ///////////////////////
+
+function createNewAccount() {
+
+}
+
+function editAccount() {
+
+}
+
+function deleteAccount() {
+
+}
+
+function  addNewChild() {
+
+}
+
+function displayGoogleSearch() {
+
+}
+
+function editChild() {
+
+}
+
+function deleteChild() {
+
+}
+
+function uploadNewAsset() {
+
+}
+
+function deleteAsset() {
+
+}
+
+function searchAssets() {
+
+}
+
+function getAndDisplayImagesOnHomePage() {
+
+}
