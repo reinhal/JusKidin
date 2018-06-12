1ff0344d9f249238476893bdd2f35f984db70c69
Lisa Reinhardt <reinal@gmail.com>
2018-06-12 13:21:51 -0500

    added html structure and styling to the child and asset html files

--- public/client.js
+++ public/client.js
@@ -26,6 +26,25 @@ function drawerOff() {
     document.getElementById("draweroverlay").style.display = "none";
 }
 
+///////////// Child(ren) Tabbed View ///////////////////////
+
+function openChild(evt, childName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(childName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
 ///////////// JusKidin Functions ///////////////////////
 
 function createNewAccount() {
